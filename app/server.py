1719aa7baff96dac9f66b0dc361ef8a7edfc0ff1
Raj Kuwar Gupta <rajkuwargupta1996@gmail.com>
2019-04-11 20:39:18 +0530

    Changed export_url

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/ilmvikzm4u6k5wi/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/ilmvikzm4u6k5wi/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['apple_pie',
